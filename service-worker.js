0afe785f53300ccc0084c65ec8fdc63df36bfd8c
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-07 09:35:27 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE='marginrush-v3pro-full';
+const CACHE='marginrush-v3-2-feedback';
 const ASSETS=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
